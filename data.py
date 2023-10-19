1e45e4b240cbac21b6b08a9fd9fe230b6cd88d0a
ines <ines.magessi@tecnico.ulisboa.pt>
2023-10-19 16:04:55 +0100

    added Bio and PBB data and changed script to read it

--- data.py
+++ data.py
@@ -1,8 +1,18 @@
-import itertools
 import numpy as np
+import pandas as pd
 
 from configs import NUMVARS
 
-dataset = np.random.randint(0, 5, size=(50, NUMVARS)).astype(float)
+# dataset = np.random.randint(0, 5, size=(250, NUMVARS)).astype(float)
 
-target = np.sum(dataset, axis=1)
+# target = np.sum(dataset, axis=1)
+
+
+PATH = 'data/Bio/test_1.csv'
+
+df = pd.read_csv(PATH, index_col = 0)
+
+target = df['target'].to_numpy()
+
+dataset = df.drop(['target'], axis = 1).to_numpy()
+
